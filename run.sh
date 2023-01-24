6e5c0830c0df31bfee9a76ea5d8a4bfd3a2c0ce0
Max Polzin <polzin.max@gmail.com>
2023-01-24 23:43:58 +0100

    Remove livox volume mount.

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ xhost +
 
 docker run -it --rm --privileged --net=host \
 --volume $(pwd)/A-LOAM:/catkin_ws/src/A-LOAM \
---volume $(pwd)/livox_ros_driver:/catkin_ws/src/livox_ros_driver \
 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
 ghcr.io/rosblox/ros-a-loam:melodic
 
